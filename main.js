fbb0bfcc9b9310637aae9565b6c7efee9f42703d
santi <santi17gallego@gmail.com>
2023-01-12 16:16:51 -0500

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navbarEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+const hamMenu = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 navbarEmail.addEventListener("click", toggleShowDesktopMenu)
+hamMenu.addEventListener("click", toggleShowMobileMenu)
+
 
 function toggleShowDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleShowMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
 }
